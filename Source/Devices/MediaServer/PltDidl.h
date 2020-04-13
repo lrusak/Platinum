whos3/xbmc

platinum: add support for upnp:episodeCount and upnp:episodeSeason from ContentDirectory v4

lib/libUPnP/Platinum/Source/Devices/MediaServer/PltDidl.h
@@ -90,6 +90,9 @@
 #define PLT_FILTER_MASK_LASTPLAYBACK                NPT_UINT64_C(0x0000000200000000)
 #define PLT_FILTER_MASK_PLAYCOUNT                   NPT_UINT64_C(0x0000000400000000)
 
+#define PLT_FILTER_MASK_EPISODE_COUNT               NPT_UINT64_C(0x0000010000000000)
+#define PLT_FILTER_MASK_EPISODE_SEASON              NPT_UINT64_C(0x0000020000000000)
+
 #define PLT_FILTER_FIELD_TITLE                      "dc:title"
 #define PLT_FILTER_FIELD_CREATOR                    "dc:creator"
 #define PLT_FILTER_FIELD_DATE                       "dc:date"
@@ -129,6 +132,9 @@
 #define PLT_FILTER_FIELD_RES_BITSPERSAMPLE          "res@bitsPerSample"
 #define PLT_FILTER_FIELD_RES_NRAUDIOCHANNELS        "res@nrAudioChannels"
 #define PLT_FILTER_FIELD_RES_SAMPLEFREQUENCY        "res@sampleFrequency"
+
+#define PLT_FILTER_FIELD_EPISODE_COUNT              "upnp:episodeCount"
+#define PLT_FILTER_FIELD_EPISODE_SEASON             "upnp:episodeSeason"
 
 extern const char* didl_header;
 extern const char* didl_footer;

lrusak/Platinum

Source/Devices/MediaServer/PltDidl.h
@@ -92,6 +92,9 @@
 
 #define PLT_FILTER_MASK_PUBLISHER                   NPT_UINT64_C(0x0000000800000000)
 
+#define PLT_FILTER_MASK_EPISODE_COUNT               NPT_UINT64_C(0x0000001000000000)
+#define PLT_FILTER_MASK_EPISODE_SEASON              NPT_UINT64_C(0x0000002000000000)
+
 #define PLT_FILTER_FIELD_TITLE                      "dc:title"
 #define PLT_FILTER_FIELD_CREATOR                    "dc:creator"
 #define PLT_FILTER_FIELD_DATE                       "dc:date"
@@ -132,6 +135,9 @@
 #define PLT_FILTER_FIELD_RES_BITSPERSAMPLE          "res@bitsPerSample"
 #define PLT_FILTER_FIELD_RES_NRAUDIOCHANNELS        "res@nrAudioChannels"
 #define PLT_FILTER_FIELD_RES_SAMPLEFREQUENCY        "res@sampleFrequency"
+
+#define PLT_FILTER_FIELD_EPISODE_COUNT              "upnp:episodeCount"
+#define PLT_FILTER_FIELD_EPISODE_SEASON             "upnp:episodeSeason"
 
 extern const char* didl_header;
 extern const char* didl_footer;